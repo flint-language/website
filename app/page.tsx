ab902b48f7407bc6a65180f1324adf4973f8bec0
Peter Griffin <codezz-ops@protonmail.com>
2025-11-28 23:51:56 -0500

    updated from vite to next

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import "./App.css";
-import logo from "./assets/amber-logo.png";
+import Image from "next/image";
 
-function App() {
+export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
-      <img src={logo} alt="Logo" className="w-64 h-auto rounded-full mb-6" />
+      <Image
+        src="/logo.png"
+        alt="Logo"
+        width={256}
+        height={256}
+        className="rounded-full mb-6"
+        priority
+      />
       <h1 className="text-4xl font-bold text-black dark:text-white text-center">
         🚧 Work in progress 🚧
       </h1>
     </main>
   );
 }
-
-export default App;

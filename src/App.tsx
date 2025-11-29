60acf27f31a141ae3fc26d1e52fb444f262f344e
CrabKing <127689590+Crab-K1ng@users.noreply.github.com>
2025-11-29 03:59:07 +0000

    add new work in progress page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css'
+import "./App.css";
+import logo from "./assets/amber-logo.png";
 
 function App() {
-  return (null)
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+      <img src={logo} alt="Logo" className="w-64 h-auto rounded-full mb-6" />
+      <h1 className="text-4xl font-bold text-black dark:text-white text-center">
+        🚧 Work in progress 🚧
+      </h1>
+    </main>
+  );
 }
 
-export default App
+export default App;
